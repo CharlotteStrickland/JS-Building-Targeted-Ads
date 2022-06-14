afb0658495e88c05bf99742e7e1d29f9e94ebb46
Charlotte Strickland <charlottemarie81@gmail.com>
2022-06-13 20:02:38 -0400

    updated app.js

--- public/app.js
+++ public/app.js
@@ -1,17 +1,40 @@
 // get user's data
-// get user's coordinates
+// get user's coordinates                                                        
+// Get the user's coordinates:                                                              
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 
+console.log(getCoords());                              
 
-// get user's time
+console.log(getCoords());
 
+// get user's time                                                             
+// Get the user's time:                                                              
+function userTime(){
+    const now = new Date()
+    return now.getHours()
+}
+console.log(userTime())                             
+                          
 
 // helper functions
 // check time of day
 
 
 // build ads
-// build ad 1
-
+// build ad 1                                                           
+function buildAd1(){
+    const mealTime = getMealTime()
+    let content = document.querySelector('.ad1')
+    let inner = document.createElement('p')
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`
+    content.append(inner)
+}
+buildAd1()
 
 // build ad 2
 
